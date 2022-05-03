5a9711e235c8826defb8c51dcba3bcc1a93a77b6
Schmelzofen <sertac.oezdemir@gmail.com>
2022-05-03 20:19:28 +0200

    profilepage

--- client/src/components/pages/Profilepage.js
+++ client/src/components/pages/Profilepage.js
@@ -1,15 +1,31 @@
 import ChangeCredentials from "../partials/ChangeCredentials";
 import ProfilePicture from "../partials/ProfilePicture";
+import UserContext from '../../components/store/contextProvider';
+import { useContext } from 'react';
 
 const Profilepage = () => {
+    const userAuth = useContext(UserContext);
+    const parsedToken = JSON.parse(userAuth.token)
+    const userView = window.location.pathname.includes(parsedToken.id)
+
     return (
-        <div className="profilePageContainer">
-            <div className="profileHeader">
-                <h1>Hey, this is your profile page. 👋</h1>
-            </div>
-            <ProfilePicture />
-            <ChangeCredentials />
-        </div>
+        <>
+            {userView ? (
+                <div className="profilePageContainer">
+                    <div className="profileHeader">
+                        <h1>Hey, this is your profile page. 👋</h1>
+                    </div>
+                    <ProfilePicture />
+                    <ChangeCredentials />
+                </div>
+            ) : (
+                <div className="profilePageContainer">
+                    <div className="profileHeader">
+                        <h1>Hey, this is someone else's profile page. 👋</h1>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
